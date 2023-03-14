b55738b3a5ac01bb163c8665f988bdc453bb47b9
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2023-03-13 20:39:43 -0500

    updated singlethought file to singlepet.js

--- client/src/pages/SinglePet.js
+++ client/src/pages/SinglePet.js
@@ -7,15 +7,15 @@ import { useQuery } from '@apollo/client';
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
 
-import { QUERY_SINGLE_THOUGHT } from '../utils/queries';
+import { QUERY_SINGLE_PET } from '../utils/queries';
 
 const SingleThought = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
-  const { thoughtId } = useParams();
+  const { petId } = useParams();
 
-  const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, {
+  const { loading, data } = useQuery(QUERY_SINGLE_PET, {
     // pass URL parameter
-    variables: { thoughtId: thoughtId },
+    variables: { petId: petId },
   });
 
   const thought = data?.thought || {};
@@ -26,9 +26,9 @@ const SingleThought = () => {
   return (
     <div className="my-3">
       <h3 className="card-header bg-dark text-light p-2 m-0">
-        {thought.thoughtAuthor} <br />
+        {pet.petAuthor} <br />
         <span style={{ fontSize: '1rem' }}>
-          had this thought on {thought.createdAt}
+          had this thought on {pet.createdAt}
         </span>
       </h3>
       <div className="bg-light py-4">
@@ -41,18 +41,18 @@ const SingleThought = () => {
             lineHeight: '1.5',
           }}
         >
-          {thought.thoughtText}
+          {pet.petText}
         </blockquote>
       </div>
 
       <div className="my-5">
-        <CommentList comments={thought.comments} />
+        <CommentList comments={pet.comments} />
       </div>
       <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <CommentForm thoughtId={thought._id} />
+        <CommentForm thoughtId={pet._id} />
       </div>
     </div>
   );
 };
 
-export default SingleThought;
+export default SinglePet;
