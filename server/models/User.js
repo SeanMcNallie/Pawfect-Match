b784e9742efa6e01f1fbc46ae3b7f7565f245186
devin <devinelliotgomez@gmail.com>
2023-03-14 19:46:25 -0500

    added in more requirements for profile feild

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,10 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const passwordValidator = function(value) {
- const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
- return regex.test(value);
-}
+const passwordValidator = function (value) {
+  const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+  return regex.test(value);
+};
 
 const userSchema = new Schema({
   username: {
@@ -17,31 +17,43 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
+    match: [/.+@.+\..+/, "Must match an email address!"],
   },
   password: {
     type: String,
     required: true,
     minlength: 8,
-    validate: [passwordValidator, 'Password must contain at least one number, one special character, and one letter.'],
+    validate: [
+      passwordValidator,
+      "Password must contain at least one number, one special character, and one letter.",
+    ],
   },
   profile: {
     name: {
-      type: String, 
-      required: true, 
+      type: String,
+      required: true,
       unique: true,
-    }
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
   savedAnimals: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Pet',
+      ref: "Pet",
     },
   ],
 });
 
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -53,6 +65,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
