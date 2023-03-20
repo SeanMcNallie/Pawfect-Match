56a907412575f8b43f891015d109f3ae7f296a45
Sean McNallie <111781366+SeanMcNallie@users.noreply.github.com>
2023-03-20 15:07:10 -0500

    updated App.js

--- client/src/components/Search/searchBar.js
+++ client/src/components/Search/searchBar.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-// const pefFinderAPI = e9MqwcrDevTJra2QLw8Pia4TNJxqZSYHlh6SiMpxl066vLoAtu
-// const petFinderKey = wto5AgU2rgGmTUWIRLuHQdQ4XgjnKnAmMbYT95HU
+
 function SearchBar({ setSearchResults }) {
     const [searchText, setSearchText] = useState('');
 
@@ -10,12 +9,13 @@ function SearchBar({ setSearchResults }) {
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
-        
+
     }
 
     return (
         <form onSubmit={onSubmitHandler}>
             <input type="text" name="searchText" value={searchText} onChange={onInputChange} />
+            
         </form>
     );
 }
