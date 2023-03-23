ef3bfe8e3f1e974d5ab3ca600e716886626e0ced
Deneene Whitaker <lisaelizrdo@gmail.com>
2023-03-23 18:29:14 -0500

    done

--- server/server.js
+++ server/server.js
@@ -14,9 +14,6 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
-
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -29,12 +26,19 @@ app.get("*", (req, res) => {
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
   });
-});
+};
 
 // Call the async function to start the server
-// startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
