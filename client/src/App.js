8f1360abcf365ebbd09db292d995722992da2e63
Deneene Whitaker <lisaelizrdo@gmail.com>
2023-03-23 18:24:16 -0500

    updated some files

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import Header from './components/Header';
 
 import Footer from './components/Footer';
 import Search from './pages/Search';
-import SearchBar from './components/Search/searchBar';
+// import SearchBar from './components/Search/searchBar';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
